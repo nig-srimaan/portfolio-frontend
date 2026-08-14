3294f2f10c344d405d66a93c59af563a6aa8a066
nig-srimaan <srimaankarra@gmail.com>
2026-08-14 21:23:40 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -29,35 +29,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
-);import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './hooks/useAuth';
-import ProtectedRoute from './components/UI/ProtectedRoute';
-import Home from './pages/Home';
-import AdminLogin from './pages/AdminLogin';
-import AdminDashboard from './pages/AdminDashboard';
-import ProjectPage from './pages/ProjectPage';
-import './styles/globals.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute>
-                <AdminDashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/project/:id" element={<ProjectPage />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  </React.StrictMode>
 );
